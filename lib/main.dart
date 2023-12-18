23e13cf9d8b5dd65878d1c4fb194ffb3f923823d
Aleksandr Ivanov <aivanov@dataeast.com>
2023-12-19 00:24:37 +0700

    Add window size constraints for Windows/Linux/MacOS

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:party_games_app/core/injection_container.dart';
 import 'package:party_games_app/party_games_app.dart';
 import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:window_manager/window_manager.dart';
 
 Future<PartyGamesAppInitData> handleIntents() async {
   var uri = await ReceiveSharingIntent.getInitialTextAsUri();
@@ -20,6 +21,9 @@ void main() async {
     sqfliteFfiInit();
     // Change the default factory
     databaseFactory = databaseFactoryFfi;
+
+    WindowManager.instance.setMinimumSize(const Size(400, 800));
+    WindowManager.instance.setMaximumSize(const Size(400, 800));
   }
   if (Platform.isAndroid) {
     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
